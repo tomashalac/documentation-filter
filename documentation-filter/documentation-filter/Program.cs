1daeb8a34f386eb0abb7913b9be1c1b1852f0fd4
Tomas halac <tomashalac@gmail.com>
2019-06-15 03:43:22 -0300

    Initial code

---
+++ documentation-filter/documentation-filter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("documentation-filter-tests")]
+namespace TomasHalac.DocumentationFilter {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Exporting the public documentation of this project!, end file: '" + Environment.CurrentDirectory + @"\new_documentation-filter.xml'");
+
+            var doc = new DocumentationFilter("documentation-filter.xml", Environment.CurrentDirectory + "/documentation-filter.dll");
+            doc.MoveOnlyPublics();
+
+        }
+    }
+}
